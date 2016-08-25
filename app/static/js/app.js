f9a49ca2a627c0a13b4a5096c0a0ad91f2832bb0
S. Cooper <staci.mcl@gmail.com>
2016-08-25 17:39:26 -0400

    Added angular routing. Moved stamp list to partial

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,12 +1,22 @@
-var stampApp = angular.module('stampApp', []);
+var stampApp = angular.module('stampApp', ['ngRoute']);
 
-stampApp.config(function($interpolateProvider){
-	// Change Angular delimiter notation so it does not conflict with Jinja2
-    $interpolateProvider.startSymbol('{a').endSymbol('a}');
+stampApp.config(function($interpolateProvider, $routeProvider) {
+		// Change Angular delimiter notation so it does not conflict with Jinja2
+        $interpolateProvider.startSymbol('{a').endSymbol('a}');
+
+        $routeProvider
+		.when('/',
+		{
+			controller: 'stampCtrl',
+			templateUrl: '../static/partials/stamp_list.html'
+		})
+		.otherwise({ redirectTo: '/' });
 });
 
 stampApp.controller('stampCtrl', function($scope, $http) {
 	$http.get('/stamp/api/v1.0/stamps').success(function(data){
 		$scope.stamps = data;
+	}).error(function(data){
+		console.log('Error getting data in stampCtrl');
 	})
 })
