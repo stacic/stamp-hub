3bfa7db8c4b855b578ff94b37d18041a0fccf238
S. Cooper <staci.mcl@gmail.com>
2016-08-25 19:48:19 -0400

    Setting up env for heroku

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 IMAGES_PATH = '/static/stamp-images/'
