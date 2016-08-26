29109577a9ae6fa27bdbfe592176f878d61a14cb
S. Cooper <staci.mcl@gmail.com>
2016-08-25 20:10:18 -0400

    Fixed db settings to allow deploying locally

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+if os.environ.get('DATABASE_URL') is None:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db') + '?check_same_thread=False'
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 IMAGES_PATH = '/static/stamp-images/'
